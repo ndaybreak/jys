575c223448f12bcd470b74e869e79f609b7b29a0
niuxiaoming <nxiaoming@163.com>
2019-01-29 18:35:36 +0800

    modify prod url

--- config/env/prod.js
+++ config/env/prod.js
@@ -2,5 +2,5 @@ module.exports = {
   NODE_ENV: '"production"',
   ENV_CONFIG: '"production"',
   BASE_API: '"/DaVaoBusiness"', // 后端接口
-  QUAT_API: '"ws://13.229.58.243:19999/quot"' // 行情接口
+  QUAT_API: '"ws://13.229.58.45:19999/quot"' // 行情接口
 }
